cb9fcf3707fabab629d1c64e95c4c2151aa98853
gugahoi <guga@maurorisch.com.br>
2011-05-18 01:05:33 +1000

    Fixed NavBar only showing correctly in FF (now works for all browsers).

--- nav.php
+++ nav.php
@@ -4,24 +4,24 @@ include_once "functions.php";
 	$settings = formatSettings($settingsDB);
 	$navlinks = $settings['navlinks'];
 
-  echo"<html>";
-  echo"  <head>";
-  echo"    <title>Navigation</title>";
-  echo"    <link rel='stylesheet' type='text/css' href='style/css/nav.css'>";
-  echo"  </head>";
-  echo"  <body>";
-  echo"    <div id=header>";
-  echo"      <div id=home>";
-  echo"        <a href='./mediafrontpage.php' target='main'></a>";
-  echo"      </div>";
-  echo"      <div id=nav-menu>";
-  echo"        <ul>";
+  echo	"<html>";
+  echo	"  <head>";
+  echo	"    <title>Navigation</title>";
+  echo	"    <link rel='stylesheet' type='text/css' href='style/css/nav.css'>";
+  echo	"  </head>";
+  echo	"  <body>";
+  echo	"    <div id=header>";
+  echo	"      <div id=home>";
+  echo	"        <a href='./mediafrontpage.php' target='main'></a>";
+  echo	"      </div>";
+  echo	"      <div id=nav-menu>";
+  echo	"        <ul>";
   foreach( $navlinks as $navlink) {
-    echo"          <li><a href='".$navlink['url']."' target=main>".$navlink['label']."</a></li>";
+    echo	"          <li><a href='".$navlink['url']."' target=main>".$navlink['label']."</a></li>";
   }
-  echo"        </ul>";
-  echo"      </div>";
-  echo"    </div>";
-  echo"  </body>";
-  echo"</html>";
+  echo	"        </ul>";
+  echo	"      </div>";
+  echo	"    </div>";
+  echo	"  </body>";
+  echo	"</html>";
 ?>
