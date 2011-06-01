355053f081fb4553de71f596dc857c8bcbd52154
Mikie-Ghost <mike.esteves@gmail.com>
2011-06-01 04:32:36 -0700

    Fix for logout

--- logout.php
+++ logout.php
@@ -1,10 +1,7 @@
 <?php
 require_once('config.php');
 $_SESSION['loggedin'] = false;
-?>
-<?php
-/* Redirect browser */
-header("Location: index.php");
-/* Make sure that code below does not get executed when we redirect. */
+echo true;
+return true;
 exit;
 ?>
