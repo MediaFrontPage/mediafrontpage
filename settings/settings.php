21244cb751f9c78aa11b39281bd85bed9685d506
Mikie-Ghost <mike.esteves@gmail.com>
2011-08-17 00:24:59 +0300

    Clean up some code

--- settings/settings.php
+++ settings/settings.php
@@ -60,23 +60,23 @@ if(!empty($_GET) && strpos($_SERVER['HTTP_REFERER'],'settings')){
 <html>
 <head>
   <title>Settings</title>
-  <link href="../css/front.css" rel="stylesheet" type="text/css">
-  <link href="../css/settings.css" rel="stylesheet" type="text/css">
-  <script type="../text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-  <script type="../text/javascript" src="js/fisheye-iutil.min.js"></script>
-  <script type="../text/javascript" src="js/settings.js"></script>
+  <link rel="stylesheet" type="text/css" href="../css/front.css"  >
+  <link rel="stylesheet" type="text/css" href="../css/settings.css"  >
+  <link rel="stylesheet" type="text/css" href="../css/jquery.pnotify.default.css">
+  <link rel="stylesheet" type="text/css" href="../css/UI/jquery-ui-1.8.14.custom.css">
   <link rel="stylesheet" type="text/css" href="../css/widget.css">
   <link rel="stylesheet" type="text/css" href="../css/static_widget.css">
   <link rel="stylesheet" type="text/css" href="../css/footer.css">
-  <script src="../js/jquery.scrollTo-1.3.3-min.js" type="text/javascript"></script>
-  <script src="../js/jquery.localscroll-1.2.5-min.js" type="text/javascript"></script>
-  <script src="../js/jquery.serialScroll-1.2.1-min.js" type="text/javascript"></script>
-  <script type="text/javascript" src="../http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>  
-  <link rel="stylesheet" type="text/css" href="../css/jquery.pnotify.default.css">
-  <link rel="stylesheet" type="text/css" href="../css/UI/jquery-ui-1.8.14.custom.css">
-  <script src="../js/jquery.pnotify.js" type="text/javascript"></script>
+  <link rel="stylesheet" type="text/css" href="../css/tipsy.css"  />
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
+  <script type="text/javascript" src="../js/fisheye-iutil.min.js"></script>
+  <script type="text/javascript" src="../js/settings.js"></script>
+  <script type="text/javascript" src="../js/jquery.scrollTo-1.3.3-min.js"></script>
+  <script type="text/javascript" src="../js/jquery.localscroll-1.2.5-min.js"></script>
+  <script type="text/javascript" src="../js/jquery.serialScroll-1.2.1-min.js"></script>
+  <script type="text/javascript" src="../js/jquery.pnotify.js"></script>
   <script type="text/javascript" src="../js/jquery.tipsy.js"></script>
-  <link rel="stylesheet" href="../css/tipsy.css" type="text/css" />
   <script type='text/javascript'>
   $(function() {
     $('input').tipsy({gravity: 'w', fade: true});
